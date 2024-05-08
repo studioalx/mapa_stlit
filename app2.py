ec73feff9698b2850a26f3bc1aeb751c322d370a
studio.alx <103322015+studioalx@users.noreply.github.com>
2024-05-08 10:51:03 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1306,8 +1306,8 @@ with tabs[2]:
 with tabs[-1]:
     col_creditos1, col_creditos2 = st.columns([1, 1], gap='large')
 
-    col_creditos1.subheader('Founded by [IRB(Re)](https://www.https://integralsolucao.com.br/)')
-    col_creditos1.caption('AA integral solução em gestão surgiu da necessidade de apresentar soluções que visam integrar e atender diferentes áreas de atuação, tanto pública quanto privada.')
+    col_creditos1.subheader('ANTONIO NERIS MACHADO JÚNIOR (https://www.https://integralsolucao.com.br/)')
+    col_creditos1.caption('A integral solução em gestão surgiu da necessidade de apresentar soluções que visam integrar e atender diferentes áreas de atuação, tanto pública quanto privada.')
     col_creditos1.image('int.jpg', use_column_width=True)
 
     col_creditos2.subheader('Developed by Instituto de Riscos Climáticos')
